9da902f44305a8933ea933d82e3e6b4a99cb4fe2
Nisa Agni <nisa.agniafifah230@gmail.com>
2024-06-03 12:27:13 +0700

    Push

--- src/Components/Berita/Berita.jsx
+++ src/Components/Berita/Berita.jsx
@@ -1,6 +1,5 @@
 import ButtonNavigasi from "../Button_navigasi";
 import { useEffect, useState } from "react";
-import axios from "axios";
 import { fetchBerita } from "../../config/api_berita";
 const Berita = () => {
   const [data, setData] = useState([]);
@@ -27,10 +26,10 @@ const Berita = () => {
         <div className="flex flex-col gap-4 pb-44">
           {data.map((item) => (
             <div className="w-full h-auto flex flex-col gap-2 p-2 border-2 rounded-2xl shadow-md">
-              <img className="rounded-t-lg" src={item.img} alt="" />
+              <img className="rounded-t-lg border" src={item.img} alt="" />
               <div>
                 <h1 className="px-2 font-bold">{item.judul}</h1>
-                <p className="px-2">{item.isi}</p>
+                <p className="px-2 text-justify">{item.isi}</p>
               </div>
               <button className="bg-[#2DBAB1] rounded-md py-2 font-bold text-white">
                 Selanjutnya
